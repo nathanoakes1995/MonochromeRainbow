22b0ea64299291d1ac9a861541a40837cd389328
nathanoakes1995 <o014892c@student.staffs.ac.uk>
2014-12-14 01:51:56 +0000

    Added Scene array

--- MonochromeRainbow/AppMain.cs
+++ MonochromeRainbow/AppMain.cs
@@ -15,7 +15,7 @@ namespace MonochromeRainbow
 	public class AppMain
 	{
 		private static Sce.PlayStation.HighLevel.GameEngine2D.Scene 	gameScene;
-		private static Sce.PlayStation.HighLevel.UI.Scene 				uiScene;
+		private static Sce.PlayStation.HighLevel.UI.Scene[] 				menuScene;
 		private static Sce.PlayStation.HighLevel.UI.Label				scoreLabel;
 		private static Sce.PlayStation.HighLevel.UI.Label				healthLabel;
 		private static Sce.PlayStation.HighLevel.UI.Label				ammoLabel;
@@ -89,7 +89,11 @@ namespace MonochromeRainbow
 			gameScene.Camera.SetViewFromViewport();
 			
 			//Set the ui scene.
-			uiScene = new Sce.PlayStation.HighLevel.UI.Scene();
+			menuScene = new Sce.PlayStation.HighLevel.UI.Scene[2];
+			for(int i = 0; i < 2; i++)
+			{
+				menuScene[i] = new Sce.PlayStation.HighLevel.UI.Scene();
+			}
 			Panel panel  = new Panel();
 			panel.Width  = Director.Instance.GL.Context.GetViewport().Width;
 			panel.Height = Director.Instance.GL.Context.GetViewport().Height;
@@ -133,9 +137,12 @@ namespace MonochromeRainbow
 				90 - rainbowLabel.Width/2,
 				Director.Instance.GL.Context.GetViewport().Height*0.1f - rainbowLabel.Height/2 + 20);
 			panel.AddChildLast(rainbowLabel);
+			for(int i = 0; i < 2; i++)
+			{
+				menuScene[i].RootWidget.AddChildLast(panel);
+				UISystem.SetScene(menuScene[i]);	
+			}
 			
-			uiScene.RootWidget.AddChildLast(panel);
-			UISystem.SetScene(uiScene);
 			
 			enemy = new Enemy[20];
 			
